c177f9843376c0d1c9852b42ad37937dad930208
Anton Savenok <reg.docent@gmail.com>
2018-03-25 23:51:27 +0400

    add default project

---
+++ app/src/main/java/com/docenttsr/views/xfallview/TestActivity.java
@@ -0,0 +1,16 @@
+package com.docenttsr.views.xfallview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_test);
+    }
+
+}
