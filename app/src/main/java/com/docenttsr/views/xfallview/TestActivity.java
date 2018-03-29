2febae2b2a986e46c3431fff8e195e3ee3c18cf3
Anton Savenok <reg.docent@gmail.com>
2018-03-30 00:27:01 +0400

    activities - add test activity

--- app/src/main/java/com/docenttsr/views/xfallview/TestActivity.java
+++ app/src/main/java/com/docenttsr/views/xfallview/TestActivity.java
@@ -1,16 +1,59 @@
 package com.docenttsr.views.xfallview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 
 public class TestActivity extends AppCompatActivity {
+
+    private static final int DELAY_BEFORE_ANIMATION = 500;
+
+    private Handler handler;
+
+    private XFallView xFallView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_test);
+
+        initViews();
+
+        handler = new Handler();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                xFallView.startFall();
+
+                xFallView.animate()
+                        .setDuration(500)
+                        .alpha(1)
+                        .start();
+            }
+        }, DELAY_BEFORE_ANIMATION);
+    }
+
+    @Override
+    protected void onStop() {
+        xFallView.stopFall();
+
+        handler.removeCallbacksAndMessages(null);
+
+        super.onStop();
+    }
+
+    private void initViews() {
+        xFallView = findViewById(R.id.test_xfall_view);
+
+        xFallView.setAlpha(0.f);
     }
 
 }
